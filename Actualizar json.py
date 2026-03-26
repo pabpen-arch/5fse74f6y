7cd812ce8e7632a3a2f290ca013b1e4a870e19f5
pabpen-arch <pabpen@fincomercio.com>
2026-03-26 15:40:45 -0500

    Add files via upload

--- Actualizar json.py
+++ Actualizar json.py
@@ -1,10 +1,18 @@
 import pandas as pd
 import json
 import numpy as np
+import os
+from openpyxl import load_workbook
+
+# ==========================================================
+# Función para obtener la ruta base
+# ==========================================================
+def ruta_relativa(*sub_ruta):
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), *sub_ruta))
 
 # 🔵 Rutas
-excel_path = r"C:\Users\usu-calfnc_37\Documents\Datamarshall\4.Canales\3.Canal Virtual\Consolidado_Cierre_Autogestionables.xlsx"
-output_path = r"C:\Users\usu-calfnc_37\Documents\Datamarshall\1.Scripts\DigitalCommandCenter_HTML\data.json"
+excel_path = ruta_relativa('..', '..', '4.Canales', '3.Canal Virtual', 'Consolidado_Cierre_Autogestionables.xlsx')
+output_path = ruta_relativa('..', '..', '1.Scripts', 'DigitalCommandCenter_HTML', 'data.json')
 
 # Leer Excel
 df = pd.read_excel(excel_path, engine="openpyxl")
@@ -17,13 +25,14 @@ df["Fecha"] = pd.to_datetime(df["Fecha"]).dt.strftime("%Y-%m-%d")
 
 # 🔵 Columnas numéricas a convertir a entero
 numeric_columns = [
+    "Meta",
     "Valor Total",
     "Valor Autogestionado",
     "Valor Asesor",
     "Prom Monto",
     "Plazo Promedio",
     "Prom Monto Autogestionado",
-    "Plazo Prom Autogestionado",
+    "Plazo Prom Auto",
     "Prom Monto Asesor",
     "Plazo Prom Asesor"
 ]
@@ -45,6 +54,8 @@ for product_name, group in df.groupby("Indicador"):
         monthly_data.append({
             "mes": row["Fecha"],
 
+            "meta": row["Meta"],
+
             "valorTotal": row["Valor Total"],
             "valorAutogestionado": row["Valor Autogestionado"],
             "valorAsesor": row["Valor Asesor"],
@@ -53,7 +64,7 @@ for product_name, group in df.groupby("Indicador"):
             "plazoPromedio": row["Plazo Promedio"],
 
             "promMontoAutogestionado": row["Prom Monto Autogestionado"],
-            "plazoPromAutogestionado": row["Plazo Prom Autogestionado"],
+            "plazoPromAutogestionado": row["Plazo Prom Auto"],
 
             "promMontoAsesor": row["Prom Monto Asesor"],
             "plazoPromAsesor": row["Plazo Prom Asesor"]
